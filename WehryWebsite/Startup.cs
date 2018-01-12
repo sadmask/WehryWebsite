634535b29c774fbe0b65f4db2239990820568db6
PJ <31600032+sadmask@users.noreply.github.com>
2018-01-11 20:19:12 -0500

    Add project files.

---
+++ WehryWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WehryWebsite.Startup))]
+namespace WehryWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
